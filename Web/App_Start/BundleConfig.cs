106b6f2cecdcb5d7a27961ac735c77f420abd3b2
Maxim Golovashevich <golovashevich@gmail.com>
2013-05-03 13:20:24 +0300

    Organizing scripts Get rid of waste files

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -23,7 +23,13 @@ namespace Web
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.css",
+				"~/Content/bootstrap-responsive.css",
+				"~/Content/font-awesome.css",
+				"~/Content/css.css",
+				"~/Content/base.css",
+				"~/Content/blue.css"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
 						"~/Content/themes/base/jquery.ui.core.css",
